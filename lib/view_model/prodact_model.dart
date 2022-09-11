0dacc236d31b8e5251d42ddac39e8f5cee8bff01
zein elabdeen <zeinelabdeen21@gmail.com>
2022-09-11 13:42:57 +0200

    add server get

--- lib/view_model/prodact_model.dart
+++ lib/view_model/prodact_model.dart
@@ -1,12 +1,10 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:mvvm_provider_practice/model/model.dart';
 import 'package:mvvm_provider_practice/repository/api_product.dart';
 
 class ProductListViewModel with ChangeNotifier {
   List<ProductData> list = [];
-  // ProductsModel model = ProductsModel();
   Future<void> getData() async {
     try {
       list = await ApiProducts().fetchData();
